6de741f4faa6ee94c3529da20537b96bf0e85f6c
Daniel_Ward <danielward27@outlook.com>
2020-07-10 13:09:57 +0100

    Update migration_length_1 prior

--- scripts/priors.py
+++ scripts/priors.py
@@ -8,14 +8,14 @@ from sim.utils import test_prior
 
 np.random.seed(2)
 
-# Set up distributions
+# Set up distributions.
 dist = {
     "pop_size_domestic_1": ss.uniform(100, 10000),  # ss.uniform(100, 15000),
     "pop_size_wild_1": ss.lognorm(0.4, scale=np.exp(8)),  # ss.lognorm(0.4, scale=np.exp(8.5)),
     "pop_size_captive": ss.lognorm(0.5, scale=np.exp(4.5)),
     "captive_time": ss.lognorm(0.7, scale=np.exp(3)),
-    "migration_length_1": ss.lognorm(0.7, np.exp(3)),  # ss.uniform(0, 0) to remove
-    "migration_rate_1": ss.beta(2, 50),  # ss.uniform(0, 0) to remove
+    "migration_length_1": ss.lognorm(0.7, scale=np.exp(3)),
+    "migration_rate_1": ss.beta(2, 50),
     "bottleneck_strength_domestic": ss.uniform(0, 40000),
     "bottleneck_time_domestic": ss.norm(3500, 600),
     "pop_size_domestic_2": ss.uniform(1000, 20000),  # ss.uniform(1000, 25000),
@@ -31,7 +31,6 @@ runs = 200000
 df = pd.DataFrame({"random_seed": np.arange(1, runs + 1)})
 
 for key, d in dist.items():
-    # Sample from the prior
     df[key] = d.rvs(runs)
 
     # Plot PDFs
@@ -41,7 +40,7 @@ for key, d in dist.items():
     plt.subplots_adjust(left=0.1, bottom=0.1)
     plt.xlabel(key)
     plt.ylabel("Probability density")
-    plt.savefig("../plots/prior_distributions/{}.png".format(key))
+    plt.savefig("../plots/prior/{}.png".format(key))
     plt.clf()
 
 float_col_names = ["migration_rate_1", "migration_rate_2"]
