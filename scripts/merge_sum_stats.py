f338c6238bb3273bcb0afb5aeec4e768ade18d3e
Daniel Ward <danielward27@outlook.com>
2021-01-12 12:01:42 +0000

    update rejection abc scripts

--- scripts/merge_sum_stats.py
+++ scripts/merge_sum_stats.py
@@ -1,22 +1,22 @@
 import pandas as pd
 import numpy as np
+import pyarrow
 
 df_list = []
 missing_files = []
-num_files = 2180
+num_files = 400
 
-for i in range(0, num_files):
+for i in range(1, num_files):
     filename = "../output/rejection/summary_stats/summary_stats_{}.feather".format(i)
     try:
         df = pd.read_feather(filename)
         df = df.reset_index(drop=True)
         df_list.append(df)
-    except FileNotFoundError:
+    except pyarrow.lib.ArrowIOError:
         missing_files.append(i)
 
 sum_stats = pd.concat(df_list, axis=0).reset_index(drop=True)
 sum_stats = sum_stats.sort_values(by="random_seed")
-sum_stats = sum_stats.drop(columns="all_pops_afs_median")  # Had all zeros - not too sure why?
 
 # Check everything look right
 seeds = sum_stats["random_seed"]
@@ -28,10 +28,5 @@ if len(missing_files) != 0:
 
 if len(difs) != 0:
     print("Warning: np.setdiff1d suggests missing seeds")
-
-
-sum_stats = sum_stats.rename(columns=replacement_dict)    
-
-    
 
 sum_stats.to_csv("../output/summary_stats.csv", index=False)
