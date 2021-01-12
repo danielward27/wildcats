ecd7f99cde3a490419b120dc021e124075d410d8
Daniel Ward <danielward27@outlook.com>
2021-01-12 12:14:18 +0000

    increase time on rej_submitter

--- scripts/rejection_submitter.sh
+++ scripts/rejection_submitter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # request resources:
 #PBS -N run_sim_0_99
-#PBS -l nodes=1:ppn=1,walltime=100:00:00
+#PBS -l nodes=1:ppn=1,walltime=150:00:00
 #PBS -l mem=8gb 
 #PBS -t 0-99
 #PBS -o ../output/logs/error/
